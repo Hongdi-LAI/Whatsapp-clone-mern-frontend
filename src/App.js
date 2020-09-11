d5c034a73edb4f6a190446a9d1fd739a4524d200
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-11 23:59:17 +0200

    introduce axios for data fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from 'pusher-js';
+import axios from './axios';
 
 function App() {
+
+  const [messages,setMessage] = useState([]);
+
+  //fetching data from database
+  useEffect(() => {
+    axios.get('/messages/sync').then(response => {
+      setMessage(response.data)
+    });
+  }, []);
+  // linking database with client using Pusher
+  useEffect(() => {
+    const pusher = new Pusher('73179937325a3bd38d1a', {
+      cluster: 'eu'
+    });
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(newMessage) {
+      //alert(JSON.stringify(data));
+      setMessage([...messages, newMessage]);
+    });
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className = "app">
       <div className = "app__body">
